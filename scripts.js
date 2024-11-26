e21c9553aedf7ea2509e6d96ef72a8161263ae64
Cedween <mikolajhipzz@gmail.com>
2024-11-26 19:21:45 +0100

    update

--- scripts.js
+++ scripts.js
@@ -1,50 +1,39 @@
-// Function to load language data from JSON file
 function loadLanguage(lang) {
     fetch(`/translations/${lang}.json`)
         .then(response => response.json())
         .then(data => {
-            // Update page content with translated text
             document.getElementById('pageTitle').innerText = data.pageTitle;
             document.getElementById('menuAbout').innerText = data.menu.about;
-            document.getElementById('menuCertificates').innerText = data.menu.certificates;
             document.getElementById('menuProjects').innerText = data.menu.projects;
             document.getElementById('menuContact').innerText = data.menu.contact;
             document.getElementById('sectionAbout').innerText = data.sections.about.title;
             document.getElementById('aboutContent').innerText = data.sections.about.content;
-            document.getElementById('sectionCertificates').innerText = data.sections.certificates.title;
             document.getElementById('sectionProjects').innerText = data.sections.projects.title;
             document.getElementById('sectionContact').innerText = data.sections.contact.title;
             document.getElementById('contactContent').innerText = data.sections.contact.content;
             document.getElementById('copyrightContent').innerText = data.sections.copyright.content;
-            // Update other sections similarly
         })
         .catch(error => console.error('Error loading language:', error));
 }
 
-// Function to change language and save preference
 function changeLanguage(lang) {
     loadLanguage(lang);
     localStorage.setItem('language', lang);
 }
 
-// Check if language preference is saved in localStorage
 const savedLanguage = localStorage.getItem('language');
 
-// If language preference is not saved, set default language to English ("en")
 if (!savedLanguage) {
     localStorage.setItem('language', 'en');
 }
 
-// Load language based on saved preference
-loadLanguage(savedLanguage || 'en'); // Default to English if no preference is saved
+loadLanguage(savedLanguage || 'en');
 
-// Set selected language in the language dropdown
 const languageSelect = document.getElementById('languageSelect');
 if (languageSelect) {
     languageSelect.value = savedLanguage || 'en';
 }
 
-// Listen for changes in language selection
 if (languageSelect) {
     languageSelect.addEventListener('change', function() {
         const selectedLanguage = languageSelect.value;
@@ -52,24 +41,18 @@ if (languageSelect) {
     });
 }
 
-// Funkcja do płynnego przewijania do danej sekcji
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
         window.scrollTo({
             top: section.offsetTop,
-            behavior: 'smooth' // Ustawienie płynnego przewijania
+            behavior: 'smooth'
         });
     }
 }
 
-// Obsługa kliknięcia na przyciskach sekcji
 document.getElementById('menuAbout').addEventListener('click', function() {
     scrollToSection('about');
-});
-
-document.getElementById('menuCertificates').addEventListener('click', function() {
-    scrollToSection('certificates');
 });
 
 document.getElementById('menuProjects').addEventListener('click', function() {
@@ -80,5 +63,4 @@ document.getElementById('menuContact').addEventListener('click', function() {
     scrollToSection('contact');
 });
 
-// Ustawienie aktualnego roku w elemencie o id="currentYear"
 document.getElementById("currentYear").innerText = new Date().getFullYear();
