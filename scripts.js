9f9e58d11ceab994b6202574eed92fed0a1a8972
Cedween <mikolajhipzz@gmail.com>
2026-04-20 10:31:16 +0200

    RolesOrSpecializations

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ function loadLanguage(lang) {
         .then(data => {
             document.getElementById('pageTitle').innerText = data.pageTitle;
             document.getElementById('menuAbout').innerText = data.menu.about;
-            document.getElementById('menuRolesAndSpecializations').innerText = data.menu.rolesAndSpecializations;
+            document.getElementById('menuRolesOrSpecializations').innerText = data.menu.rolesOrSpecializations;
             document.getElementById('menuProjects').innerText = data.menu.projects;
             document.getElementById('menuContact').innerText = data.menu.contact;
             document.getElementById('sectionAbout').innerText = data.sections.about.title;
             document.getElementById('aboutContent').innerText = data.sections.about.content;
-            document.getElementById('sectionRolesAndSpecializations').innerText = data.sections.rolesAndSpecializations.title;
-            document.getElementById('rolesAndSpecializationsContent').innerText = data.sections.rolesAndSpecializations.content;
+            document.getElementById('sectionRolesOrSpecializations').innerText = data.sections.rolesOrSpecializations.title;
+            document.getElementById('rolesOrSpecializationsContent').innerText = data.sections.rolesOrSpecializations.content;
             document.getElementById('sectionProjects').innerText = data.sections.projects.title;
             document.getElementById('sectionContact').innerText = data.sections.contact.title;
             document.getElementById('contactContent').innerText = data.sections.contact.content;
@@ -58,8 +58,8 @@ document.getElementById('menuAbout').addEventListener('click', function() {
     scrollToSection('about');
 });
 
-document.getElementById('menuRolesAndSpecializations').addEventListener('click', function() {
-    scrollToSection('rolesandspecializations');
+document.getElementById('menuRolesOrSpecializations').addEventListener('click', function() {
+    scrollToSection('rolesorspecializations');
 });
 
 document.getElementById('menuProjects').addEventListener('click', function() {
